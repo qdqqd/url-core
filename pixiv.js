0b7d8020f67de5a989255c1fe2dfcd08d7c96b31
by柯乐 <1010456736@qq.com>
2025-06-11 18:15:22 +0800

    Update pixiv.js

--- pixiv.js
+++ pixiv.js
@@ -3,8 +3,8 @@ async function fetchBingImages() {
     const endpoint = isWideScreen 
      //   ? 'https://api.allorigins.win/raw?url=https%3A%2F%2Fapi.lolicon.app%2Fsetu%2Fv2%3Fsize%3Dregular%26num%3D20%26aspectRatio%3Dgt1%26proxy%3Di.pximg.org' 
      //   : 'https://api.allorigins.win/raw?url=https%3A%2F%2Fapi.lolicon.app%2Fsetu%2Fv2%3Fsize%3Dregular%26num%3D20%26aspectRatio%3Dlt1%26proxy%3Dmashir0-pximg.hf.space';
- ? 'https://i.qdqqd.com/?cors=https://thingproxy.freeboard.io/fetch/https%3A%2F%2Fapi.lolicon.app%2Fsetu%2Fv2%3Fsize%3Dregular%26num%3D20%26aspectRatio%3Dgt1%26proxy%3Di.pximg.org'
- : 'https://i.qdqqd.com/?cors=https://thingproxy.freeboard.io/fetch/https%3A%2F%2Fapi.lolicon.app%2Fsetu%2Fv2%3Fsize%3Dregular%26num%3D20%26aspectRatio%3Dlt1%26proxy%3Di.pximg.org';
+ ? 'https://thingproxy.freeboard.io/fetch/https%3A%2F%2Fapi.lolicon.app%2Fsetu%2Fv2%3Fsize%3Dregular%26num%3D20%26aspectRatio%3Dgt1%26proxy%3Di.pximg.org'
+ : 'https://thingproxy.freeboard.io/fetch/https%3A%2F%2Fapi.lolicon.app%2Fsetu%2Fv2%3Fsize%3Dregular%26num%3D20%26aspectRatio%3Dlt1%26proxy%3Di.pximg.org';
  //    ? 'https://image.anosu.top/pixiv/json?num=20&proxy=i.pximg.org&size=regular'
 // : 'https://image.anosu.top/pixiv/json?num=20&proxy=i.pximg.org&size=regular';
     const response = await fetch(endpoint);
