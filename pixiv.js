6f1feb2ab087585223057fa66f59d17ddf183354
by柯乐 <1010456736@qq.com>
2026-02-06 23:28:18 +0800

    Update pixiv.js

--- pixiv.js
+++ pixiv.js
@@ -3,8 +3,8 @@
 async function fetchBingImages() {
     const isWideScreen = window.innerWidth > window.innerHeight;
     const endpoint = isWideScreen 
-        ? 'https://pixiv-api.wrnm.dpdns.org/pc_pixiv.json' 
-        : 'https://pixiv-api.wrnm.dpdns.org/pe_pixiv.json';
+        ? 'https://pixiv-api.qdqqd.com/pc_pixiv.json' 
+        : 'https://pixiv-api.qdqqd.com/pe_pixiv.json';
     
     const headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
